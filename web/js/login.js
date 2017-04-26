e95b89f212e4dde7a477fb8c39a9d0946b03248b
ZhenlyChen <zhenlychen@foxmail.com>
2017-04-26 13:28:14 +0800

    日常修BUG
    1. 登陆后隐藏登陆栏
    2. 修复登陆逻辑设计错误

--- web/js/login.js
+++ web/js/login.js
@@ -1,12 +1,6 @@
 
 function login() {
-  if(document.getElementById("user_email").value == ''){
-    sendNotice("<strong>邮箱地址为空</strong>  请输入邮箱地址");
-    $("#user_email").focus();
-  } else if (document.getElementById("user_password").value == '' && document.getElementById("user_email").value != ''){
-    sendNotice("<strong>密码为空</strong>  请输入密码");
-    $("#user_password").focus();
-  }
+
   if(document.getElementById("user_email").value != '' && document.getElementById("user_password").value != ''){
     sendNotice("登陆中，请稍后");
     $.post("api/login",{
